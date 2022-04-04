90f86bb032346c83b5efd4bc3fd82cbe2371a97d
khoa2520 <86915350+khoa2520@users.noreply.github.com>
2022-04-04 15:09:29 +0700

    update_config

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -8,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   constructor() { }
-
+  heart : String = 'bi-heart';
+  person : String = 'bi-person';
+  hidden : String = 'hidden';
   ngOnInit(): void {
   }
 
