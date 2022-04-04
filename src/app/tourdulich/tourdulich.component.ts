9a0a641a34df840534ff911387e08a8870368a37
thititing123 <93417411+thititing123@users.noreply.github.com>
2022-04-04 14:31:15 +0700

    update tourdulich
    data sửa đường dẫn
    interface add thêm tour.ts

--- src/app/tourdulich/tourdulich.component.ts
+++ src/app/tourdulich/tourdulich.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute, Router } from '@angular/router';
+import { TourdulichService } from '../service/tourdulich.service';
 @Component({
   selector: 'app-tourdulich',
   templateUrl: './tourdulich.component.html',
   styleUrls: ['./tourdulich.component.css']
 })
 export class TourdulichComponent implements OnInit {
-
-  constructor() { }
+  tour: any;
+  errorMsg:string="";
+  constructor(private _service: TourdulichService) { }
 
   ngOnInit(): void {
+    this._service.getDataTour().subscribe(
+      {
+        next:(data)=>this.tour=data,
+        error:(err)=>this.errorMsg=err.message
+      })
   }
 
 }
