0ca819d6403bc88fa8057a8c1603ae895c07103c
khoa2520 <86915350+khoa2520@users.noreply.github.com>
2022-04-07 22:14:57 +0700

    Update_Header_DangKy_TrangChu
    (Khoa test thử format push code lên github)

--- src/app/dang-ky/dang-ky.component.ts
+++ src/app/dang-ky/dang-ky.component.ts
@@ -10,20 +10,29 @@ import { customValidator, passwordValidator } from '../Checkformat/check.validat
 export class DangKyComponent implements OnInit {
 
   public regForm: FormGroup = this._formBuilder.group({
-    hoTen: ['Đăng khoa', [Validators.required, Validators.minLength(3), customValidator]],
-    gTinh: ['ngdakhoa@gmail.com', [Validators.required,Validators.email]],
-    ngaySinh: [''],
-    diDong: [''],
-    cMND:[''],
+    hoTen: ['Nguyễn Văn A', [Validators.required,Validators.minLength(3), customValidator]],
+    // gTinh: ['ngdakhoa@gmail.com', [Validators.required,Validators.email]],
+    ngaySinh: [10/10/2001],
+    diDong: ['0326123397', [ Validators.required,
+      Validators.pattern("[- +()0-9]+"),
+      Validators.minLength(10), Validators.maxLength(10)]],
+    cMND:['352588958', [ Validators.required,Validators.pattern(/^-?(0|[1-9]\d*)?$/)]],
     ngayCap:[''],
-    email:[''],
-    diaChi:[''],
-    pass:[''],
+    email:['ngdakhoa@gmail.com', [ Validators.required, Validators.email]],
+    diaChi:['115 Đinh Chiến Thắng', [ Validators.required]],
+    pass:['',[ Validators.required]],
     confirmPass:[''],
   }, {validators: [passwordValidator]})
   // })
   constructor(private _formBuilder: FormBuilder) { }
-
+  keyPress(event: any) {
+    const pattern = /[0-9\+\-\ ]/;
+ 
+    let inputChar = String.fromCharCode(event.charCode);
+    if (event.keyCode != 8 && !pattern.test(inputChar)) {
+      event.preventDefault();
+    }
+  }
   ngOnInit(): void {
   }
 
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,9 +7,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  btnHeart: string = 'bi-heart';
+  btnPerson: string = 'bi-person';
+  hidden: string = 'hidden';
+  hidden_dm: string='hidden_dm'
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  changeHeart(){
+    if(this.btnHeart=='bi-heart')
+    {
+      this.btnHeart = 'bi-heart-fill';
+    }
+    else{
+      this.btnHeart='bi-heart'
+    }
+  }
+  changePerson(){
+    if(this.btnPerson=='bi-person')
+    {
+      this.btnPerson = 'bi-person-fill';
+    }
+    else{
+      this.btnPerson='bi-person'
+    }
+  }
+  changeHidden(){
+    if(this.hidden=='hidden')
+    {
+      this.hidden = '';
+    }
+    else{
+      this.hidden='hidden'
+    }
+  }
+
+  changeHidden_dm(){
+    if(this.hidden_dm=='hidden_dm'){
+      this.hidden_dm = 'hidden'
+    }
+  }
+  changeDefault(){
+    this.hidden_dm = 'hidden_dm'
+  }
 }
