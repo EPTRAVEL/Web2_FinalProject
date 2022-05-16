5bdef9ea790976978b8bd93f2ae11a945c44de1f
ThyLe1705 <thylmn19406c@st.uel.edu.vn>
2022-05-16 23:30:12 +0700

    NT Update_Data KhuyenMai
    Update phần data trang Khuyến Mãi

--- src/app/khuyenmai/khuyenmai.component.ts
+++ src/app/khuyenmai/khuyenmai.component.ts
@@ -1,3 +1,4 @@
+import { TourdulichService } from './../service/tourdulich.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +7,63 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./khuyenmai.component.css']
 })
 export class KhuyenmaiComponent implements OnInit {
-
-  constructor() { }
+  tour: any;
+  errorMsg: string = "";
+  
+ 
+  slideConfig = {
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    dots: true,
+    // autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 900,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 650,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+  
+  slickInit(e: any) {
+    console.log('slick initialized');
+  }
+  breakpoint(e: any) {
+    console.log('breakpoint');
+  }
+  afterChange(e: any) {
+    console.log('afterChange');
+  }
+  beforeChange(e: any) {
+    console.log('beforeChange');
+  }
+  constructor(private _service:TourdulichService) { }
 
   ngOnInit(): void {
+    this._service.getDataTour().subscribe(
+      {
+        next: (data) => this.tour = data,
+        error: (err) => this.errorMsg = err.message
+      })
   }
 
 }
